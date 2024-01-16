6feca30b563512049419d93b4d8e29c0524aceee
Rafa Kuchta <rafal.kuchta1@gmail.com>
2024-01-16 14:11:33 +0100

    Added console log

--- dolor.js
+++ dolor.js
@@ -1,4 +1,5 @@
 console.log("Dolor sit amet!");
 const result = 10 + 10;
 console.log('Calculating...')
-console.log(`Result is ${result}`)
+console.log(`Result is ${result}`);
+console.log('Lorem ipsum');
